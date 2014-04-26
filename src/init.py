c769673351d24b59edbe2f8ec76ab88fe22ee7d6
Abhishek Sharma <jkabhishek88@gmail.com>
2014-04-25 20:27:48 -0500

    Abhi| #0 | Implementing DS for Gibbs Sampling

--- src/init.py
+++ src/init.py
@@ -1,9 +1,19 @@
 __author__ = 'abhi21'
 
-n = 3
+#import numpy as num
+import random
+
+n = 2
 modelDict = {}
 keysDict ={}
-context = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+totalIncrements = 0
+global index
+index = -1
+characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+allContexts = []
+allChars = []
+allTables = []
 
 def initializeList():
     freq = {'a':[0],'b':[0],'c':[0],'d':[0],'e':[0],'f':[0],'g':[0],'h':[0],'i':[0],'j':[0],'k':[0],'l':[0],'m':[0],'n':[0],'o':[0],'p':[0],'q':[0],'r':[0],'s':[0],'t':[0],'u':[0],'v':[0],'w':[0],'x':[0],'y':[0],'z':[0]}
@@ -15,7 +25,7 @@ def makeKey(k,n):
         keysDict[k] = k
     else:
         keysDict[k] = k
-        for item in context:
+        for item in characters:
             itemK = item + k
             makeKey(itemK, n-1)
 
@@ -23,22 +33,6 @@ def getSubStr(beginIndex, sliceWindow, word):
     return word[beginIndex: beginIndex + sliceWindow]
 
 
-def updateFrequencyOld(word, n):
-    word = word.lower()
-    lenWord = len(word)
-    for i in range(0, lenWord):
-        for sliceWindow in range(1,n):
-            noOfRemChars = len(word[i:])
-            if (noOfRemChars >= sliceWindow):
-                subStr = getSubStr(i, sliceWindow, word)
-                lenSubStr = len(subStr)
-                if lenSubStr == 1:
-                    modelDict[''][subStr][0] += 1
-                else:
-                    context = subStr[:lenSubStr-1]
-                    char = subStr[lenSubStr -1:lenSubStr]
-                    #print(char)
-                    modelDict[context][char][0] += 1
 
 makeKey('',n)
 
@@ -51,18 +45,49 @@ for key in keysDict:
 def updateFrequency(word, n):
     word = word.lower()
     wordLen = len(word)
+    nTable = 0
 
     for i in range(0, wordLen):
         tempN = n
         psc = i
-        while((psc >= 0) and (tempN-1 >=0)):
+
+        while((psc >= 0) and (tempN - 1 >=0)):
+            global index
+            index = index + 1
             psc = psc-1
             tempN = tempN - 1
             context = word[psc + 1:i]
-            #3print('Context:' + context)
+            #print('Context:' + context)
             char = word[i:i + 1]
             #print('Char: ' + char)
             modelDict[context][char][0] += 1
+            allContexts.append(context)
+            allChars.append(char)
+            nTable = len(modelDict[context][char]) - 1
+            tableNumber = random.randrange(0, nTable+1,1)
+            if tableNumber in modelDict[context][char][1:nTable]:
+                modelDict[context][char][tableNumber] += 1
+            else:
+                modelDict[context][char].append(1)
+            allTables.append(tableNumber)
+
+
+def initializeTables(word):
+    word = word.lower()
+    wordLen = len(word)
+
+    for i in range(0, wordLen):
+        tempN = n
+        psc = i
+        while((psc >= 0) and (tempN - 1 >=0)):
+            psc = psc-1
+            tempN = tempN - 1
+            context = word[psc + 1:i]
+            #print('Context:' + context)
+            char = word[i:i + 1]
+            #print('Char: ' + char)
+            modelDict[context][char][0] += 1
+
 
 
 #updateFrequency('AbhiAb',n)
@@ -77,20 +102,33 @@ def updateFrequency(word, n):
 #print(modelDict['h']['i'][0])
 #print(modelDict['']['i'][0])
 
-modelDict['a']['a'][0] = modelDict['a']['a'][0] + 2
-print(modelDict['a']['a'][0])
+#modelDict['a']['a'][0] = modelDict['a']['a'][0] + 2
+#print(modelDict['a']['a'][0])
+#
+#for key in keysDict:
+#    print('\nKey: ' + key)
+#    #print(modelDict[key])
 
-with open("data/dist_all_last.txt", "r") as f:
+def assertInitialization():
+    for key in keysDict:
+        for char in characters:
+            freq = modelDict[key][char][0]
+            sumCustomers = sum(modelDict[key][char])-freq
+            print('Freq: ' + str(freq) + 'Customers' + str(sumCustomers))
+
+
+with open("data/dist_female_first.txt", "r") as f:
   for line in f:
     name = line.split()[0]
-    updateFrequency(name,4)
-    print(name)
+    updateFrequency(name,n)
+  assertInitialization()
 
-for key in keysDict:
-    print('\nKey: ' + key + ": value= ")
-    print(modelDict[key])
+#print(totalIncrements)
 
-print(modelDict['aa']['r'])
+#or key in keysDict:
+#   print('\nKey: ' + key)
+#   print(modelDict[key])
+
 
 
 
--- src/test.py
+++ src/test.py
@@ -18,3 +18,23 @@ def generateNgrams(word, n):
 inputWord = 'Hello!!'
 print(len(inputWord))
 generateNgrams(inputWord,3)
+
+
+def updateFrequencyOld(word, n, totalIncrements):
+    word = word.lower()
+    lenWord = len(word)
+    for i in range(0, lenWord):
+        for sliceWindow in range(1,n):
+            noOfRemChars = len(word[i:])
+            if (noOfRemChars >= sliceWindow):
+                subStr = getSubStr(i, sliceWindow, word)
+                lenSubStr = len(subStr)
+                if lenSubStr == 1:
+                    modelDict[''][subStr][0] += 1
+                    totalIncrements +=1
+                else:
+                    context = subStr[:lenSubStr-1]
+                    char = subStr[lenSubStr -1:lenSubStr]
+                    #print(char)
+                    modelDict[context][char][0] += 1
+                    totalIncrements +=1
